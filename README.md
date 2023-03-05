85e2132799d665e1d54529dc6756992cc1fd4fb9
William <william.mallevays@epitech.eu>
2023-03-05 23:36:08 +0100

    {-} Clean man

--- README.md
+++
@@ -1,5 +0,0 @@
-# R-Type
-
-## Documentation
-
-You can see our documentation in the documentation folder. Use vscode to open a better view, on a live server.
--- includes/Network/includes/network.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ctime>
-#include <iostream>
-#include <string>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-#include <boost/bind/bind.hpp>
-#include <boost/shared_ptr.hpp>
